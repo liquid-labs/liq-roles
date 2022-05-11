e9b47a49ee8c671046313fdf2b7af56056228c66
Zane Rockenbaugh <zane@liquid-labs.com>
2022-05-11 15:51:46 -0500

    refactored 'chd access' report to support dynamic sections

--- src/handlers/orgs/roles/access/_transforms/chd-access.js
+++ src/handlers/orgs/roles/access/_transforms/chd-access.js
@@ -1,14 +1,26 @@
-const chdRoleSets = [
-  {
-    roles: [ 'DevOps Engineer', 'Production Administrator' ],
-    summary: 'Inclusive of Network, Host, and Cloud Services Engineers'
+const sections = {
+  'Technical operations': {
+    summary: 'These staff memebrs manage and have access to critical systems within and enabling the CDE.',
+    roleSets: [
+      {
+        roles: [ 'DevOps Engineer', 'Production Administrator' ],
+        summary: 'Inclusive of Network, Host, and Cloud Services Engineers'
+      },
+      { roles: [ 'Database Engineer', 'Production Administrator' ] },
+      {
+        roles: [ 'MOCA Administrator' ],
+        summary: 'Staff with administrative access to the MOCA administrative portal.'
+      }
+    ]
   },
-  /* { roles: [ 'Network Engineer', 'Production Administrator' ] },
-  { roles: [ 'Host Engineer', 'Production Administrator' ] },
-  { roles: [ 'Cloud Engineer', 'Production Administrator' ] }, */
-  { roles: [ 'Database Engineer', 'Production Administrator' ] },
-  { roles: [ 'Customer Service Agent' ] }
-]
+  'Support and business operations': {
+    summary: 'These staff access and process transactions on behalf of the customer or internal business processes.',
+    roleSets: [
+      { roles: ['Customer Service Agent'] },
+      { roles: ['Settlement Agent'] }
+    ]
+  }
+}
 
 const chdAccess = ({
   allRoles=false,
@@ -19,18 +31,30 @@ const chdAccess = ({
   res,
   rolesAccess
 }) => {
-  let report = '# Cardholder Data Access Report\n\n'
-  for (const { summary, roles } of chdRoleSets) {
-    report += `## ${roles[0]}s\n${summary ? `__${summary}__\n` : ''}\n`
-    const staff = org.staff.getByRoleName(roles)
-    if (!staff || staff.length === 0) {
-      report += '__NONE__\n\n'
-    }
-    else {
-      for (const staffMember of staff) {
-        report += `- ${staffMember.getFullName()} <${staffMember.email}>\n`
+  let report = `# PCI DSS Roles and Access Report
+
+## Purpose and scope
+
+This document details which staff members directly responsible for the proper handling and security of cardholder data and the cardholder data environment (CDE).
+
+`
+  for (const section of Object.keys(sections)) {
+    const { summary, roleSets } = sections[section]
+    report += `## ${section}\n\n${summary}\n\n`
+
+    for (const { summary, roles } of roleSets) {
+      report += `### ${roles[0]}s\n${summary ? `__${summary}__\n` : ''}\n`
+      const staff = org.staff.getByRoleName(roles)
+      if (!staff || staff.length === 0) {
+        report += '__NONE__\n\n'
       }
-      report += '\n'
+      else {
+        for (const staffMember of staff) {
+          if (staffMember.employmentStatus !== 'logical')
+          report += `- ${staffMember.getFullName()} <${staffMember.email}>\n`
+        }
+        report += '\n'
+      }
     }
   }
   
