a42be53661f172027fc32307e74b1408155940a2
Zane Rockenbaugh <zane@liquid-labs.com>
2022-09-15 09:49:49 -0500

    added option to display direct staff roles or decomposed

--- src/handlers/orgs/roles/policies/policies-by-role.mjs
+++ src/handlers/orgs/roles/policies/policies-by-role.mjs
@@ -3,13 +3,29 @@ import { getDocumentsByRoles, getPoliciesIndexByRole } from '@liquid-labs/liq-ro
 
 const method = 'post'
 const path = '/orgs/:orgKey/roles/policies/reviews-by-roles'
-const parameters = []
+const parameters = [
+  {
+    name: 'directOnly',
+    isBoolean: true,
+    description: `Creates a map of direct policy implications; i.e., drops the implied roles layer. Notice that in this case, the output has format:
+\`\`\`
+{ <role>: [list of documetns],...}
+\`\`\`
+rather than standard:
+\`\`\`
+{ <role>: { <implied role>: [list of documents ] }, ...}, ...
+\`\`\`
+`
+  }
+]
 
 const func = ({ app, model }) => (req, res) => {
   const org = getOrgFromKey({ model, params: req.params, res })
   if (org === false) { // getOrgFromKey handles sending error response, if any
     return
   }
+  
+  const { directOnly } = req.query
   
   const rolePoliciesIndex = getPoliciesIndexByRole({ org, res })
   if (rolePoliciesIndex === false) return // error response handled by lib function
@@ -18,9 +34,20 @@ const func = ({ app, model }) => (req, res) => {
     
   const policyDocsByRole = {}
   for (const roleName of roleNames) {
-    const policyDocs = getDocumentsByRoles({ org, roleNames: [roleName], rolePoliciesIndex })
-    if (policyDocsByRole === false) return // ran into error already handled by lib funtion
-    policyDocsByRole[roleName] = policyDocs
+    if (directOnly === true || directOnly === 'true') {
+      const policyDocs = getDocumentsByRoles({ org, res, roleNames: [roleName], rolePoliciesIndex })
+      if (policyDocsByRole === false) return // ran into error already handled by lib funtion
+      policyDocsByRole[roleName] = policyDocs
+    }
+    else { // we break down the actual source of the docs
+      const role = org.roles.get(roleName)
+      const subMap = {}
+      policyDocsByRole[roleName] = subMap
+      for (const subRoleName of [roleName, ...role.allImpliedRoleNames]) {
+        subMap[subRoleName] = getDocumentsByRoles({ org, res, roleNames: [subRoleName], rolePoliciesIndex })
+        if (subMap[subRoleName] === false) return // ran into error already handled by lib funtion
+      }
+    }
   }
   
   res.json(policyDocsByRole)
