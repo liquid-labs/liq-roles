4637e8237349aa94a3494416c01dd87150a255dc
Zane Rockenbaugh <zane@liquid-labs.com>
2022-09-15 11:36:10 -0500

    reworked parameters; directOnly -> directPolicyOnly; added 'staffedOnly' and 'decompose'; updated docs and impl to support

--- src/handlers/orgs/roles/policies/policies-by-role.mjs
+++ src/handlers/orgs/roles/policies/policies-by-role.mjs
@@ -1,3 +1,6 @@
+/**
+* Displays a list of policy documents attached to (required reading for) each role in the org structure.
+*/
 import { getOrgFromKey } from '@liquid-labs/liq-handlers-lib'
 import { getDocumentsByRoles, getPoliciesIndexByRole } from '@liquid-labs/liq-roles-lib'
 
@@ -5,17 +8,33 @@ const method = 'post'
 const path = '/orgs/:orgKey/roles/policies/reviews-by-roles'
 const parameters = [
   {
-    name: 'directOnly',
+    name: 'decompose',
     isBoolean: true,
-    description: `Creates a map of direct policy implications; i.e., drops the implied roles layer. Notice that in this case, the output has format:
+    description: `Decomposes the policy implications for each role by implied role; i.e., the format goes from:
 \`\`\`
 { <role>: [list of documetns],...}
 \`\`\`
-rather than standard:
+to the decomposed version:
 \`\`\`
 { <role>: { <implied role>: [list of documents ] }, ...}, ...
 \`\`\`
-`
+
+When decomposed, only direct requirements are shown for each decomposed role.`
+  },
+  {
+    name: 'directPoliciesOnly',
+    isBoolean: true,
+    description: "Rather than the default behavior of showing all documents attached to the role (directly and through implication), when `directPoliciesOnly` is set true, only the policy requirements directly defined for the role will be displayed. E.g., a 'Lead Engineer' would only show the additional requirements of being a lead and not the requirements inheritted from a base (implied) 'Engineer' role. Note that decomposed roles are always direct only."
+  },
+  {
+    name: 'includeImpliedRoles',
+    isBoolean: true,
+    description: "When `includeImpliedRoles` is true, then all roles, not just those defined in the org structure, are considered."
+  },
+  {
+    name: 'staffedOnly',
+    isBoolean: true,
+    description: "If true, then only those roles which are staffed are included in the report."
   }
 ]
 
@@ -25,21 +44,28 @@ const func = ({ app, model }) => (req, res) => {
     return
   }
   
-  const { directOnly } = req.query
+  const { decompose, directPoliciesOnly, includeImpliedRoles, staffedOnly } = req.body || {}
   
   const rolePoliciesIndex = getPoliciesIndexByRole({ org, res })
   if (rolePoliciesIndex === false) return // error response handled by lib function
   
-  const roleNames = org.roles.list({ rawData: true }).map((r) => r.name)
+  const staffListOptions = { rawData: true}
+  // TODO: I don't think the string test is necessary anymore
+  if (includeImpliedRoles === true || includeImpliedRoles === 'true') {
+    staffListOptions.includeIndirect = true
+  }
+  const roleNames = org.roles.list(staffListOptions).map((r) => r.name)
     
   const policyDocsByRole = {}
+  const getStaffOpts = includeImpliedRoles
+    ? { impliedRoles : true }
+    : {}
   for (const roleName of roleNames) {
-    if (directOnly === true || directOnly === 'true') {
-      const policyDocs = getDocumentsByRoles({ org, res, roleNames: [roleName], rolePoliciesIndex })
-      if (policyDocsByRole === false) return // ran into error already handled by lib funtion
-      policyDocsByRole[roleName] = policyDocs
-    }
-    else { // we break down the actual source of the docs
+    if ((staffedOnly === true || staffedOnly === 'true')
+        && org.roles.getStaffInRole(roleName, getStaffOpts).length === 0)
+      continue
+    
+    if (decompose === true || decompose === 'true') {
       const role = org.roles.get(roleName)
       const subMap = {}
       policyDocsByRole[roleName] = subMap
@@ -47,6 +73,21 @@ const func = ({ app, model }) => (req, res) => {
         subMap[subRoleName] = getDocumentsByRoles({ org, res, roleNames: [subRoleName], rolePoliciesIndex })
         if (subMap[subRoleName] === false) return // ran into error already handled by lib funtion
       }
+    }
+    else {
+      let policyRoles
+      if (directPoliciesOnly === true || directPoliciesOnly === 'true') {
+        policyRoles = [ roleName ]
+      }
+      else {
+        const role = org.roles.get(roleName)
+        policyRoles = [ roleName, ...role.allImpliedRoleNames ]
+      }
+        
+      const policyDocs = getDocumentsByRoles({ org, res, roleNames: policyRoles, rolePoliciesIndex })
+      if (policyDocsByRole === false) return // ran into error already handled by lib funtion
+      
+      policyDocsByRole[roleName] = policyDocs
     }
   }
   
