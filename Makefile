c948fb0c7aac57a46b979b6ff89060a46072a208
Zane Rockenbaugh <zane@liquid-labs.com>
2022-11-02 15:47:27 -0500

    non-working and messy attempt at server side implementation of the org chart

--- Makefile
+++ Makefile
@@ -6,21 +6,27 @@ default: build
 NPM_BIN:=$(shell npm bin)
 CATALYST_SCRIPTS:=$(NPM_BIN)/catalyst-scripts
 
-LIQ_ROLES_SRC:=src/
+DIST_DIR:=dist
+
+LIQ_ROLES_SRC:=src
 LIQ_ROLES_FILES:=$(shell find $(LIQ_ROLES_SRC) \( -name "*.js" -o -name "*.mjs" \) -not -path "*/test/*" -not -name "*.test.js")
 LIQ_ROLES_TEST_SRC_FILES:=$(shell find $(LIQ_ROLES_SRC) -name "*.js")
 LIQ_ROLES_TEST_BUILT_FILES:=$(patsubst $(LIQ_ROLES_SRC)/%, test-staging/%, $(LIQ_ROLES_TEST_SRC_FILES))
-LIQ_ROLES:=dist/liq-roles.js
+LIQ_ROLES:=$(DIST_DIR)/liq-roles.js
 
 # browser files
-CANVAS_HTML=dist/canvas.html
-CANVAS_SRC=src/handlers/orgs/roles/lib/canvas.html
-D3_JS=dist/d3.v7.js
-D3_URL=https://d3js.org/d3.v7.js
-D3_FLEXTREE_JS=dist/d3-flextree.js
-D3_FLEXTREE_URL=https://cdn.jsdelivr.net/npm/d3-flextree@2.1.2/build/d3-flextree.js
+CANVAS_HTML:=$(DIST_DIR)/canvas.html
+CANVAS_SRC:=src/handlers/orgs/roles/lib/canvas.html
 
-BROWSER_FILES:=$(CANVAS_HTML) $(D3_JS) $(D3_FLEXTREE_JS)
+D3_JS:=$(DIST_DIR)/d3.v7.js
+D3_URL:=https://d3js.org/d3.v7.js
+D3_FLEXTREE_JS:=$(DIST_DIR)/d3-flextree.js
+D3_FLEXTREE_URL:=https://cdn.jsdelivr.net/npm/d3-flextree@2.1.2/build/d3-flextree.js
+JSPDF_JS:=$(DIST_DIR)/jspdf.umd.min.js
+JSPDF_URL:=https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js
+# d3-org-chart.js is loaded directly from node_modules
+
+BROWSER_FILES:=$(CANVAS_HTML) $(D3_JS) $(D3_FLEXTREE_JS) $(D3_FLEXTREE_JS).map $(JSPDF_JS) $(JSPDF_JS).map
 
 BUILD_TARGETS:=$(LIQ_ROLES) $(BROWSER_FILES)
 
@@ -40,6 +46,15 @@ $(D3_JS):
 	
 $(D3_FLEXTREE_JS):
 	curl $(D3_FLEXTREE_URL) --output $(D3_FLEXTREE_JS)
+
+$(D3_FLEXTREE_JS).map:
+	curl $(D3_FLEXTREE_URL).map --output $(D3_FLEXTREE_JS).map
+
+$(JSPDF_JS):
+	curl $(JSPDF_URL) --output $(JSPDF_JS)
+
+$(JSPDF_JS).map:
+	curl $(JSPDF_URL).map --output $(JSPDF_JS).map
 
 # test build and run rules
 $(LIQ_ROLES_TEST_BUILT_FILES) &: $(LIQ_ROLES_TEST_SRC_FILES)
--- src/handlers/orgs/roles/org-chart.mjs
+++ src/handlers/orgs/roles/org-chart.mjs
@@ -1,6 +1,13 @@
 import * as fs from 'fs/promises'
 
-import puppeteer from 'puppeteer'
+// import * as d3 from 'd3'
+import D3Node from 'd3-node'
+import { OrgChart } from 'd3-org-chart'
+import canvasModule from 'canvas'
+// import { JSDOM, VirtualConsole } from 'jsdom'
+// import puppeteer from 'puppeteer'
+
+import { getOrgFromKey } from '@liquid-labs/liq-handlers-lib'
 
 const method = 'get'
 const path = '/orgs/:orgKey/roles/org-chart'
@@ -15,12 +22,81 @@ let browser = undefined
 
 const func = ({ model }) => async (req, res) => {
   const { orgKey } = req.params
+  const org = getOrgFromKey({ model, params: req.params, res })
+  if (org === false) { // I think 'getOrgFromKey' generates the error
+    return
+  }
+  
+  const staff = org.staff.list()
+  const data = []
+  for (const staffMember of staff) {
+    for (const role of staffMember.getOwnRoles()) {
+      if (role.designated) continue
+      const roleName = role.name
+      if (roleName === 'Staff' || roleName === 'Contractor' || roleName === 'Employee') continue
+      
+      const datum = {
+        id : staffMember.email + '/' + role.name,
+        email: staffMember.email,
+        name: `${staffMember.givenName} ${staffMember.familyName} <${staffMember.email}>`,
+        title: role.name
+      }
+      if (role.managerEmail) {
+        datum.parentId = role.managerEmail + '/' + role.managerRole
+      }
+      
+      data.push(datum)
+    }
+  }
+  
+  const d3n = new D3Node({ canvasModule })      // initializes D3 with container element
+  // console.log(d3)
+  const svg = d3n.createSVG(10,20).append('g')
+  // OrgChart expects to find these
+  global.window = d3n.window
+  global.document = d3n.document
+  global.navigator = {
+    maxTouchPoints: null
+  }
+  
+  global.d3 = d3n.d3 // TODO: necessary?
+  // console.log(d3n)
+  console.log(d3n.html())
+  /*
+  const virtualConsole = new VirtualConsole();
+  virtualConsole.sendTo(console)
+  const dom = new JSDOM('<!DOCTYPE html><body><p id="main"></body>)', { pretendToBeVisual: true, virtualConsole })
+  const window = dom.window
+  // global.window = window
+  global.document = window.document
+  
+  var vis = d3.select(window.document).select('body').html('').append('svg')
+    .attr('xmlns', 'http://www.w3.org/2000/svg')
+    .attr('xmlns:xlink', 'http://www.w3.org/1999/xlink')
+    .attr('width', 800)// width + pad.l + pad.r)
+    .attr('height', 600)// height + pad.t + pad.b)
+    .append('g')
+  
+  // const container = d3.select(window.document).select("svg g")
+  console.log(vis)
+  // console.log(container)
+  */
+  const container = d3n.d3.select('svg').element
+  console.log(container)
+  var chart = new OrgChart()//new OrgChart()
+    .container('svg')
+    .data(data)
+    .render()
+
+  console.log(await chart.exportImg())
+  
   // const org = getOrgFromKey({ model, orgKey, params: req.params, res })
   
+  /*
   const { interactive = false } = req.query
   
   // yes, we repeat org key, but it makes it easy to retrieve from the HTML page.
-  const pageUrl = `http://127.0.0.1:32600/orgs/${orgKey}/roles/org-chart/page?${orgKey}`
+  const pageUrl = `http://127.0.0.1:32600/orgs/${orgKey}/roles/org-chart/page`
   
   if (browser === undefined) {
     const options = {
@@ -58,6 +134,7 @@ const func = ({ model }) => async (req, res) => {
   res.send(await page.content())
   
   if (!interactive) browser.close()
+  */
 }
 
 export { func, method, parameters, path }
